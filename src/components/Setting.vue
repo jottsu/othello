baf86e9d2f8b9252412b14c6f6ab524e152cd91b
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-08-16 20:47:41 +0900

    create setting page

---
+++ src/components/Setting.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="setting">
+    <h1>Setting</h1>
+    <div>
+      先手（黒）
+      <input type="text" placeholder="名前" v-model="player1">
+    </div>
+    <div>
+      後手（白）
+      <input type="text" placeholder="名前" v-model="player2">
+    </div>
+    <button @click="startGame">はじめる</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'setting',
+  data: function() {
+    return {
+      player1: '',
+      player2: ''
+    }
+  },
+  methods: {
+    startGame: function() {
+      localStorage.setItem('player1', this.player1)
+      localStorage.setItem('player2', this.player2)
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Setting from '@/components/Setting'
 
 Vue.use(Router)
 
@@ -8,8 +8,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/setting'
+    }, {
+      path: '/setting',
+      name: 'Setting',
+      component: Setting
     }
   ]
 })
