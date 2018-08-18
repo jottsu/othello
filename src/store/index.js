f60d16c5cf9a51b4a09441c881456b9345c1a3b8
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-08-18 18:27:07 +0900

    use vuex-persistedstate to save state to Local Storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
 
@@ -24,5 +25,6 @@ export default new Vuex.Store({
       commit('setNames', names)
       commit('setIsPlaying', true)
     }
-  }
+  },
+  plugins: [createPersistedState()]
 })
