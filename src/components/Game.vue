1416163ad5591ba874b79ca8188cf0950f37d604
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2018-08-18 20:11:18 +0900

    create Board component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,11 +5,14 @@
       <span>Player1: {{ names.player1 }}</span>
       <span>Player2: {{ names.player1 }}</span>
     </div>
+    <Board></Board>
     <button @click="finishGame">おわる</button>
   </div>
 </template>
 
 <script>
+import Board from '@/components/modules/Board'
+
 export default {
   data: function() {
     return {
@@ -21,6 +24,9 @@ export default {
       this.$store.dispatch('finishGame')
       this.$router.push('setting')
     }
+  },
+  components: {
+    Board
   }
 }
 </script>
---
+++ src/components/modules/Board.vue
@@ -0,0 +1,60 @@
+<template>
+  <table class="borad">
+    <tr v-for="i in 8" :key="i">
+      <td v-for="j in 8" :key="j" @click="setDisc(i, j)">
+        {{ getDisc(i, j) }}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      discs: [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, -1, 1, 0, 0, 0,
+        0, 0, 0, 1, -1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+      ],
+      nextDisc: 1
+    }
+  },
+  methods: {
+    getDisc: function (i, j) {
+      const index = (i - 1) * 8 + j - 1
+      switch (this.discs[index]) {
+        case 1:
+          return 1
+        case -1:
+          return -1
+        default:
+          return 0
+      }
+    },
+    setDisc: function (i, j) {
+      const index = (i - 1) * 8 + j - 1
+      if (this.discs[index] !== 0) {
+        return
+      }
+      this.discs.splice(index, 1, this.nextDisc)
+      this.nextDisc *= -1
+    }
+  }
+}
+</script>
+
+<style scoped>
+.borad {
+  margin: 20px auto;
+}
+
+td {
+  cursor: pointer;
+}
+</style>
